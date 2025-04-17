ca6412497c28375259ed22aa4ce12b761259ede2
TheGomez01 <159203345+TheGomez01@users.noreply.github.com>
2025-04-17 10:11:52 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+let isMobile = /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 
+if (!isMobile) {
+  /* set navbar class to mobile one */
+  /* alert("This website is tailored to mobile devices.") */
+} else {
+  /* set navbar class to desktop one */
+}
