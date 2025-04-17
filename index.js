5cfb00b161ba3e1cae2fd32b488e4a1df32d3cdc
TheGomez01 <159203345+TheGomez01@users.noreply.github.com>
2025-04-17 10:36:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,3 +6,14 @@ if (!isMobile) {
 } else {
   /* set navbar class to desktop one */
 }
+
+//Find the button using the getElement method
+var contactButton = document.getElementById("button1");
+
+contactButton.addEventListener("click", function () {
+    //Adds an event listener that awaits the button click. Returns an alert notification and clears the fields.
+    alert("Thank you for your inquiry. We will reach out to you as soon as possible.");
+    document.getElementById("name1").value = "";
+    document.getElementById("email1").value = "";
+    document.getElementById("message1").value = "";
+});
