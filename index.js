bb08925042963890ea5e25627768ee2fb344545a
TheGomez01 <159203345+TheGomez01@users.noreply.github.com>
2025-05-21 18:59:25 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,11 @@ hamburger.addEventListener("click", () => {
   navMenu.classList.toggle("active");
 })
 
+document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
+  hamburger.classList.remove("active");
+  navMenu.classList.remove("active");
+}))
+
 //Find the button using the getElement method
 var contactButton = document.getElementById("button1");
 
